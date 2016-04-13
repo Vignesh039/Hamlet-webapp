300f5400d45b0618d9d743f78b3e3f27d91f4361
vigneshwar <vigneshwar.reddy@mediamint.com>
2016-04-13 17:50:06 +0530

    qa branch

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,7 +1,7 @@
 angular.module("zippr.config", [])
 
 .constant("ZIPPR_ENVIRONMENT", {
-	"server": "http://hamletdev.zip.pr/",
+	"server": "http://hamletqa.zip.pr/",
 	"apikey": "d338667c0196254de1922668f5b1344ba80e54fd0ef481f09cef5336b6471a99"
 })
 
